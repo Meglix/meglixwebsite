c3f1373a898d1def61d8b16bd4f6bf6ef9ca0abc
Meglix <robertmagalau@gmail.com>
2025-06-20 15:14:02 +0300

    2

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,27 +1,29 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-    content: [
-      './pages/**/*.{js,ts,jsx,tsx,mdx}',
-      './components/**/*.{js,ts,jsx,tsx,mdx}',
-      './app/**/*.{js,ts,jsx,tsx,mdx}',
-    ],
-    theme: {
-      extend: {
-        fontFamily: {
-          'nm': ['Neue Montreal', 'sans-serif'],
+export default {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        'nm': ['Neue Montreal', 'sans-serif'],
+        'sans': ['Neue Montreal', '-apple-system', 'BlinkMacSystemFont', 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'sans-serif'],
+      },
+      colors: {
+        meglix: {
+          black: '#000000',
+          white: '#FFFFFF',
+          pink: {
+            400: '#f472b6',
+            500: '#ec4899',
+            600: '#db2777',
+          },
         },
-        colors: {
-          meglix: {
-            black: '#000000',
-            white: '#FFFFFF',
-            pink: {
-              400: '#f472b6',
-              500: '#ec4899',
-              600: '#db2777',
-            },
-          }
-        },
+        background: "var(--background)",
+        foreground: "var(--foreground)",
       },
     },
-    plugins: [],
-  }
+  },
+  plugins: [],
+};
