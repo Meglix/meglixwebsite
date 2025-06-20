f532fa9f8b9b623ff35f814f52b8d77ff682544a
St4r <pauloprisor2@gmail.com>
2025-06-20 16:41:24 +0300

    config

--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,14 @@
 
 import { useState, useEffect } from "react";
 import { motion, AnimatePresence } from "framer-motion";
-import MeglixLanding from "./components/landingpage";
 
 export default function Home() {
-
   return (
     <div>
-      <MeglixLanding />
+      <div className="H1 text-black">H1</div>
+      <div className="H2 text-black">H2</div>
+      <div className="H3 text-black">H3</div>
+      <div className="body-small text-black">Body Small</div>
     </div>
   );
 }
