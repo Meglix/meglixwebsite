ca0f14717b50a07971352f9f5c7d6d439350f95a
Meglix <robertmagalau@gmail.com>
2025-06-21 14:04:08 +0300

    d

--- app/components/common/Layout.jsx
+++ app/components/common/Layout.jsx
@@ -4,9 +4,9 @@ import { useState } from "react";
 
 export default function GridOverlay({}) {
   return (
-    <div className="absolute inset-0 grid grid-cols-6 grid-rows-6 pointer-events-none z-50">
+    <div className="fixed inset-0 w-screen h-screen grid grid-cols-6 grid-rows-6 pointer-events-none z-50">
       {Array.from({ length: 36 }, (_, i) => (
-        <div key={i} className="border border-black "></div>
+        <div key={i} className="border border-black w-full h-full"></div>
       ))}
     </div>
   );
